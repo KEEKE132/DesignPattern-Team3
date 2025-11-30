303d93bee951daf8914a33939e56ad1f29be792e
hyoinkang <hyoin1704@gmail.com>
2025-12-01 02:35:00 +0900

    feat: UI패널에 보너스 아이템 획득 메세지 띄우는 기능 추가

--- src/java/game/entities/items/scoreItems/Strawberry.java
+++ src/java/game/entities/items/scoreItems/Strawberry.java
@@ -3,6 +3,6 @@ package game.entities.items.scoreItems;
 public class Strawberry extends ScoreItem {
 
     public Strawberry(int xPos, int yPos) {
-        super(xPos, yPos, 300, "img/strawberry.png");
+        super(xPos, yPos, 300, "img/strawberry.png", "SWEET STRAWBERRY!");
     }
 }
